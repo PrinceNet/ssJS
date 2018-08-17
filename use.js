94af0b676da2294783e72cc5702d2e22a424d5d0
Doron Mor <mobclown@gmail.com>
2018-08-17 22:43:06 +0300

    add simple use via code only

--- use.js
+++ use.js
@@ -1,1 +1,10 @@
+
+ss.init({
+    lang: 'en-GB',          // DOMString
+    pitch: 1,               // float 0 - 2
+    rate: 1,                // float 0.1 - 10
+    text: 'This is a test', // DOMString
+    volume: 1,              // float 0 - 1
+}).getVoices().speak();
+
 console.log(ss);
