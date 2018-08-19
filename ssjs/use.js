cb20ee0a80d18df3269f6fa23ff778581092b934
itama <itama@DESKTOP-J5MTMI0>
2018-08-19 15:06:32 +0300

    added components

--- ssjs/use.js
+++ ssjs/use.js
@@ -1,10 +1,23 @@
-
+var matches = document.querySelectorAll("[class*='ssComponent']");
+console.log(matches)
+// default utteran
 ss.init({
-    lang: 'en-GB',          // DOMString
-    pitch: 1,               // float 0 - 2
-    rate: 1,                // float 0.1 - 10
-    text: 'This is a test', // DOMString
-    volume: 1,              // float 0 - 1
+  lang: 'en-GB', // DOMString
+  pitch: 1, // float 0 - 2
+  rate: 1, // float 0.1 - 10
+  text: 'test', // DOMString
+  volume: 1, // float 0 - 1
 }).getVoices().speak();
 
-console.log(ss);
+for (var i = 0; i < matches.length; i++) {
+  console.log('matches:', matches[i].textContent);
+  ss.setUtterance({
+    lang: 'en-GB', // DOMString
+    pitch: 1, // float 0 - 2
+    rate: 1, // float 0.1 - 10
+    text: matches[i].textContent,
+    volume: 1, // float 0 - 1
+  }).getVoices().speak();
+}
+
+console.log('ssComponents:', ss.components);
